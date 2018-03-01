778c39d97c33d52865828a9ba203eb797fe1f6d5
Jean Mertz <jean@mertz.fm>
2018-03-01 09:24:46 +0100

    Add comment to allow for future removal of money patch

--- lib/excon/addressable/parser.rb
+++ lib/excon/addressable/parser.rb
@@ -7,6 +7,7 @@ module Excon
     # Parses a url using `Addressable`, setting the port to the inferred_port.
     #
     # @see : https://github.com/excon/excon/issues/384#issuecomment-42645517
+    # @see : https://github.com/excon/excon/issues/384#issuecomment-362618298
     #
     class Parser
       def self.parse(url)
